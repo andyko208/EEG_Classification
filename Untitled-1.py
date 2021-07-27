dae3be3444f9ff113c315fac26c7f1e241eb7f85
Andy Ko <peanut61313@gmail.com>
2021-07-27 17:50:26 -0400

    initiating minecraft commands

---
+++ Untitled-1.py
@@ -0,0 +1,34 @@
+import pyautogui
+import pydirectinput
+
+
+import random
+import time
+import pynput
+import winsound
+# from pywinauto import keyboard
+
+duration = 1000  # milliseconds
+freq = 440  # Hz
+pydirectinput.moveTo(100, 150)
+# pydirectinput.click(button='right')
+pydirectinput.press('esc')
+
+# mouse = pynput.mouse.Controller()
+
+# time.sleep(5)
+# winsound.Beep(freq, duration)
+
+# print('The current pointer position is {0}'.format(mouse.position))
+# mouse.move(226, 382)
+
+# winsound.Beep(freq, duration)
+
+# print(pyautogui.size())
+# print(pyautogui.onScreen(x, y))
+
+# rand_interval = random.randint()
+# pyautogui.typewrite('Hello world!\n', interval=0.01)
+
+# time.sleep(5)
+# pyautogui.moveTo(226, 382, duration=1)
