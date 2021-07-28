8405f17dd80180335fc9718481e00fea6b2231ca
Andy Ko <peanut61313@gmail.com>
2021-07-28 18:10:03 -0400

    minecraft controls

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,27 +4,49 @@ import pydirectinput
 
 import random
 import time
-# import pynput
 import winsound
+import pynput
 # from pywinauto import keyboard
 
 duration = 1000  # milliseconds
 freq = 440  # Hz
 
-# time.sleep(5)
-# pydirectinput.press('w')
-# # pyautogui.click()
-# time.sleep(0.5)
+time.sleep(5)
+
+# pydirectinput.click()
+# pydirectinput.press('t')
 # pydirectinput.click(button='right')
-# pydirectinput.moveTo(250, 350)
-# winsound.Beep(freq, duration)
+# pydirectinput.mouseUp(150, 250, button='right', duration=0.5)
 
-# mouse = pynput.mouse.Controller()
 
-# time.sleep(5)
-# print('The current pointer position is {0}'.format(mouse.position))
-# mouse.move(1036, 382)
-# winsound.Beep(freq, duration)
+x = pydirectinput.size()[0]
+y = pydirectinput.size()[1]
+
+print(x)
+print(y)
+
+""" (x = horizontal, y = vertical) """
+go_by = 450
+
+pydirectinput.moveTo(int(x/2), y)
+pydirectinput.moveTo(int(x/2), y)
+winsound.Beep(freq, duration)
+print('The current pointer position is {0}'.format(pydirectinput.position()))
+time.sleep(2)
+# pydirectinput.moveTo(int(x/2), int(y/2))
+pydirectinput.moveTo(150, 320)
+print('The current pointer position is {0}'.format(pydirectinput.position()))
+# time.sleep(0.5)
+# pydirectinput.moveTo(980, 350 + go_by)
+# pydirectinput.click()
+# print('The current pointer position is {0}'.format(pydirectinput.position()))
+
+# pydirectinput.mouseUp(150, 250, button='right', duration=0.5)
+
+
+
+# mouse.move(1036, 382)w
+winsound.Beep(freq, duration)
 
 
 
@@ -34,8 +56,3 @@ freq = 440  # Hz
 # print(pyautogui.onScreen(x, y))
 
 # rand_interval = random.randint()
-# pyautogui.typewrite('Hello world!\n', interval=0.01)
-
-# time.sleep(5)
-# pyautogui.moveTo(226, 382, duration=1)
-# pyautogui.press('enter')
