1aba7d7a45ea87184c432f11feb9718053d66564
Andy Ko <peanut61313@gmail.com>
2021-08-02 16:33:35 -0400

    commands fix

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
 import pyautogui
-import pydirectinput
+import pyautogui
 
 import random
 import time
@@ -9,67 +9,73 @@ import sys
 """ Always centers cursor at x: 1280 y: 782"""
 
 def curr_cur():
-    print('Current cursor x: {} y: {}'.format(pydirectinput.position()[0], pydirectinput.position()[1]))
+    print('Current cursor x: {} y: {}'.format(pyautogui.position()[0], pyautogui.position()[1]))
     # print('Current cursor x: {} y: {}'.format(pyautogui.position()[0], pyautogui.position()[1]))
 
 duration = 1000  # milliseconds
 freq = 440  # Hz
 
-x_size = int(pydirectinput.size()[0])
-y_size = int(pydirectinput.size()[1])
-print('Size of the window: {}'.format(pydirectinput.size()))
+x_size = int(pyautogui.size()[0])
+y_size = int(pyautogui.size()[1])
+print('Size of the window: {}'.format(pyautogui.size()))
 
 time.sleep(3)
 
-x = int(pydirectinput.position()[0]+200)
-y = int(pydirectinput.position()[1])
 
-print('Moving to x: {} y:{}'.format(x, y))
 
-# pydirectinput.moveTo(x, y)
+# print('Moving to x: {} y:{}'.format(x, y))
+
+# pyautogui.moveTo(x, y)
 curr_cur()
 winsound.Beep(freq, duration)
 
-
-""" real-time cursor coordinates"""
-# import pyautogui, sys
-# print('Press Ctrl-C to quit.')
-# try:
-#     while True:
-#         x, y = pyautogui.position()
-#         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
-#         print(positionStr, end='')
-#         print('\b' * len(positionStr), end='', flush=True)
-# except KeyboardInterrupt:
-#     print('\n')
-
 def wandering():
+
+    
     moves = ('a', 'd', 'space')
-    try:
-        t_init = time.time()
-        dur = 5
-        while (time.time() - t_init) < dur:
-            pydirectinput.keyDown('w')
-            move_var = random.choice(moves)
-            x = int((x_size / 2) + ((x_size / 100) * 5))
-            y = int((y_size / 2) - ((y_size / 100) * 5))
-            pydirectinput.moveTo(x, y)
-            pydirectinput.moveTo(x, y)
-            pydirectinput.press(move_var)
-            # if state != 'wandering':
-            #     break
-    except KeyboardInterrupt:
-        pass
+    turns = (150, -150)
+    t_init = time.time()
+    dur = 10
+    while (time.time() - t_init) < dur:
+        pyautogui.keyDown('w')
+        move_var = random.choice(moves)
+        x = int(pyautogui.position()[0])
+        y = int(pyautogui.position()[1])
+        turn_var = int(random.choice(turns))
+        pyautogui.moveTo(x+turn_var, y)
+        pyautogui.press(move_var)
+        # curr_cur()
+        # if state != 'wandering':
+        #     break
+    pyautogui.keyUp('w')
 
 def mining():
     t_init = time.time()
-    dur = 3
+    dur = 5
     while (time.time() - t_init) < dur:
-        pydirectinput.mouseDown(button='left')
+        # pyautogui.
+        pyautogui.mouseDown(button='left')
     # if state != 'mining':
     #     break
-    pydirectinput.mouseUp(button='left')
+    pyautogui.mouseUp(button='left')
 
 if __name__ == "__main__":
-    mining()
+    # mining()
+    # wandering()
+    x = int(pyautogui.position()[0])
+    # y = int(pyautogui.position()[1]+250)
+    y = int(y_size)
+    pyautogui.moveTo(x, y)
+    
+
+    """ real-time cursor coordinates"""
+    # print('Press Ctrl-C to quit.')
+    # try:
+    #     while True:
+    #         x, y = pyautogui.position()
+    #         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+    #         print(positionStr, end='')
+    #         print('\b' * len(positionStr), end='', flush=True)
+    # except KeyboardInterrupt:
+    #     print('\n')
 
